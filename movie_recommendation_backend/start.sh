b4909935add8f3d481ebffea077b1858371505ab
iliasradouche <iliasradouche2@gmail.com>
2025-09-17 19:55:47 +0100

    Add automatic movie data population to Railway deployment

--- movie_recommendation_backend/start.sh
+++ movie_recommendation_backend/start.sh
@@ -9,5 +9,8 @@ python manage.py collectstatic --noinput
 # Run migrations
 python manage.py migrate
 
+# Populate database with movie data if empty
+python manage.py shell -c "from movies.models import Movie; import sys; sys.exit(0 if Movie.objects.count() > 0 else 1)" || python manage.py populate_movies --pages 3
+
 # Start the Django application with Gunicorn
 gunicorn movie_backend.wsgi:application --bind 0.0.0.0:$PORT
